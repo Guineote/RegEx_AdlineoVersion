e8e7c22424503866020682856b54f5d6cda5a137
Guineo <61438557+Guineote@users.noreply.github.com>
2023-10-17 00:13:13 -0600

    Create RegEx.py
    Esqueleto principal de prueba de la clase

---
+++ RegEx.py
@@ -0,0 +1,20 @@
+class RegEx:
+    def __init__(self):
+        pass
+    
+    
+    def bus_simple(self):
+        pass
+    
+    
+    def rangos_n_corchetes(self):
+        pass
+    
+    
+    def buscar(self):
+        pass
+    
+    
+    def buscar_remplazar(self):
+        pass
+    
